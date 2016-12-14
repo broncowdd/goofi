6b03d6453330059bc196d282b49565ce2001c590
Bronco <broncowdd@users.noreply.github.com>
2016-12-14 10:32:41 +0100

    #1 #2

--- goofi.php
+++ goofi.php
@@ -1,7 +1,8 @@
 <?php 
-
+header("Content-Type: text/css");
+$font_folder='fonts';
 if (!empty($_GET['family'])){
-	if (!is_dir('fonts')){mkdir('fonts');}
+	if (!is_dir($font_folder)){mkdir($font_folder);}
 	$family=strip_tags($_GET['family']);
 	$css_filename=urlencode($family).'.css';
 	$css_file_url='http://fonts.googleapis.com/css?family='.urlencode($family);
@@ -12,16 +13,15 @@ if (!empty($_GET['family'])){
 
 		foreach($urls['url'] as $nb=>$url){
 			$font_file=$urls['name'][$nb].basename($url);
-			if (!is_file('fonts/'.$font_file)){
+			if (!is_file($font_folder.'/'.$font_file)){
 				$font=file_get_contents($url);
-				file_put_contents('fonts/'.$font_file, $font);
+				file_put_contents($font_folder.'/'.$font_file, $font);
 			}
-			$css=str_replace($url,'fonts/'.$font_file,$css);
+			$css=str_replace($url,$font_folder.'/'.$font_file,$css);
 		}
 		file_put_contents($css_filename,$css );
 	}else{
 		$css=file_get_contents($css_filename);
 	}
-	header("Content-Type: text/css");
 	exit($css);
 }
