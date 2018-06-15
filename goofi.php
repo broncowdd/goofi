0504963aa5bf6d044b220389ebd20c728991bec7
Bronco <broncowdd@users.noreply.github.com>
2018-06-15 15:10:19 +0200

    #3 thx @jerrywham

--- goofi.php
+++ goofi.php
@@ -1,18 +1,17 @@
-<?php 
 header("Content-Type: text/css");
 $font_folder='fonts';
 if (!empty($_GET['family'])){
 	if (!is_dir($font_folder)){mkdir($font_folder);}
-	$family=strip_tags($_GET['family']);
-	$css_filename=urlencode($family).'.css';
-	$css_file_url='http://fonts.googleapis.com/css?family='.urlencode($family);
+	$family=str_replace(array("\0",'%20'), array('','+'),rawurlencode(strip_tags($_GET['family'])));
+	$css_filename=$family.'.css';
+	$css_file_url='http://fonts.googleapis.com/css?family='.$family;
 	if (!is_file($css_filename)){
 		$css=file_get_contents($css_file_url);
 		
 		preg_match_all("#font-family: '(?P<name>[^']+)[^^]*?url\((?P<url>[^\)]+)\)#", $css, $urls);
 
 		foreach($urls['url'] as $nb=>$url){
-			$font_file=$urls['name'][$nb].basename($url);
+			$font_file=str_replace(array("\0",'%20'), array('','+'),rawurlencode($urls['name'][$nb])).basename($url);
 			if (!is_file($font_folder.'/'.$font_file)){
 				$font=file_get_contents($url);
 				file_put_contents($font_folder.'/'.$font_file, $font);
